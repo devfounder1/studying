acc8e269746aae376a7ccf8b8c7f901bda60e1ee
SH_cat <noo_noo1999@mail.ru>
2026-02-16 18:29:03 +0300

    dsaasddsaasdasd

---
+++ study.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include <vector>
+
+using namespace Eigen;
+using namespace std;
+
+
+int main() {
+  MatrixXd m(2, 2);
+  m(0, 0) = 3;
+  m(1, 0) = 2.5;
+  m(0, 1) = -10;
+  m(1, 1) = m(1, 0) + m(0, 1);
+  std::cout << m << "\n\n\n";
+
+  // define 3x3 matrix of floats 
+  Matrix<float, 3,3> matrixA;
+  matrixA.setZero();
+  cout << matrixA << "\n\n";
+  
+  // another way
+  Matrix3f matrixA1;
+  matrixA1.setZero();
+  cout << matrixA1 << "\n\n";
+
+
+
+
+
+
+
+
+
+
+
+
+}
